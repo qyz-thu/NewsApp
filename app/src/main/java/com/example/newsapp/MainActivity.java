2cf950f1733db351c2019c5cea2d8b34463cf3e4
qyz17 <qyz17@mails.tsinghua.edu.cn>
2019-08-24 15:03:15 +0800

    change the hello-world text

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String msg = "Android: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        TextView textView = findViewById(R.id.text1);
+        textView.setText(R.string.rejection);
+        Log.d(msg, "The onCreate() event.");
     }
+
 }
