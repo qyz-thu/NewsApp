ea9d7e255a64042bc2c6f88831a84332b100b271
qyz17 <qyz17@mails.tsinghua.edu.cn>
2019-08-24 19:20:43 +0800

    did some test

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String msg = "Android: ";
@@ -16,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.text1);
         textView.setText(R.string.rejection);
         Log.d(msg, "The onCreate() event.");
+
+        IntentFilter filter = new IntentFilter("com.newsapp.CUSTOM_INTENT");
+        BroadcastReceiver receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
     }
 
+    public void broadcastIntent(View view){
+        Toast.makeText(this, "button pushed.", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent();
+        intent.setAction("com.newsapp.CUSTOM_INTENT");
+        sendBroadcast(intent);
+    }
+
+    public void startService(View view)
+    {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopService(View view)
+    {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
 }
