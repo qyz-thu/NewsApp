9bc3e77c0c9039977204e68f1ce2bab03bd3f4bf
Jiajie Chen <noc@jiegec.ac.cn>
2019-08-25 11:27:44 +0800

    Implement news json parsing and use Glide to load images

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -3,27 +3,86 @@ package com.example.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
     String msg = "Android: ";
     NewsListAdapter adapter;
+    List<News> allNews;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        allNews = new ArrayList<>();
+        queue = Volley.newRequestQueue(this);
+
         TextView textView = findViewById(R.id.text1);
         textView.setText(R.string.rejection);
-        Log.d(msg, "The onCreate() event.");
+
+        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
-        recyclerView.setAdapter(adapter = new NewsListAdapter());
+        recyclerView.setAdapter(adapter = new NewsListAdapter(this));
+
+        fetchData();
+    }
+
+    void fetchData() {
+        String url = String.format("https://api2.newsminer.net/svc/news/queryNewsList?size=15&startDate=2019-07-01&endDate=2019-07-03&words=&categories=");
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("main", response.toString());
+                JSONArray data = null;
+                try {
+                    data = response.getJSONArray("data");
+                    for (int i = 0;i < data.length();i++) {
+                        allNews.add(new News(data.getJSONObject(i)));
+                    }
+                    Collections.sort(allNews);
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("main", "network", error);
+            }
+        });
+        queue.add(req);
     }
 
 }
--- app/src/main/java/com/example/newsapp/NewsListAdapter.java
+++ app/src/main/java/com/example/newsapp/NewsListAdapter.java
@@ -1,16 +1,25 @@
 package com.example.newsapp;
 
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHolder> {
 
-    private ViewGroup parent;
+    MainActivity parent;
+
+    NewsListAdapter(MainActivity parent) {
+        this.parent = parent;
+    }
 
     @NonNull
     @Override
@@ -21,18 +30,29 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull NewsListAdapter.ViewHolder holder, int position) {
-
+        News news = parent.allNews.get(position);
+        holder.titleView.setText(news.title);
+        if (!news.images.isEmpty()) {
+            Log.d("Adapter", news.images.get(0));
+            Glide.with(parent).load(news.images.get(0)).into(holder.imageView);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return parent.allNews.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        View itemView;
+        ImageView imageView;
+        TextView titleView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.itemView = itemView;
+            imageView = itemView.findViewById(R.id.imageView);
+            titleView = itemView.findViewById(R.id.titleView);
         }
     }
 }
