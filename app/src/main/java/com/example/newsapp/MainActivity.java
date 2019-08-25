45443dd5dc55976df1cb6e3d7a620a1c82d7a33c
Jiajie Chen <noc@jiegec.ac.cn>
2019-08-25 10:18:06 +0800

    Add NewsListAdapter, add basic SwipeRefresh+Recycler+StaggeredGrid in main layout

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     String msg = "Android: ";
+    NewsListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.text1);
         textView.setText(R.string.rejection);
         Log.d(msg, "The onCreate() event.");
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        recyclerView.setAdapter(adapter = new NewsListAdapter());
     }
 
 }
---
+++ app/src/main/java/com/example/newsapp/NewsListAdapter.java
@@ -0,0 +1,38 @@
+package com.example.newsapp;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHolder> {
+
+    private ViewGroup parent;
+
+    @NonNull
+    @Override
+    public NewsListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_card, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsListAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
