8a78bcc1b8aeaad372bf4831ee67610dfe617a2f
Jiajie Chen <noc@jiegec.ac.cn>
2019-09-01 21:30:55 +0800

    Use glide instead of manually loading image

--- app/src/main/java/com/example/newsapp/NewsDetailActivity.java
+++ app/src/main/java/com/example/newsapp/NewsDetailActivity.java
@@ -1,13 +1,9 @@
 package com.example.newsapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-
-import android.os.Handler;
-import android.os.Message;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -33,80 +29,32 @@ public class NewsDetailActivity extends AppCompatActivity {
     ImageView image_view;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_detail_activity);
         Intent intent = getIntent();
-        News news=null;
-        String newsid="";
+        String newsID = "";
         title_view = findViewById(R.id.news_title);
         image_view = findViewById(R.id.news_image);
         content_view = findViewById(R.id.news_content);
         date_view = findViewById(R.id.news_date);
 
-        if (intent != null)
-        {
-            newsid = intent.getStringExtra("id");
+        if (intent != null) {
+            newsID = intent.getStringExtra("id");
         }
 
         Realm realm = Realm.getDefaultInstance();
-        RealmResults<News> results = realm.where(News.class).equalTo("newsID", newsid).findAll();
-        if (results.size() != 0)
-        {
-            news = results.get(0);
-        }
-        title_view.setText(news.title);
-        content_view.setText(news.content);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        date_view.setText(format.format(news.publishTime));
-        final String url = news.images.get(0);
-        if (url != null && url.length()>0) {
-            final Handler handler = new Handler() {
-                @Override
-                public void handleMessage(Message msg) {
-                    if (msg.what == 1) {
-                        Bitmap bitmap2 = (Bitmap) msg.obj;
-                        image_view.setImageBitmap(bitmap2);
-                    }
-                }
-            };
-
-            new Thread() {
-                public void run() {
-                    Bitmap bitmap1 = getBitMap(url);
-                    Message msg = new Message();
-                    msg.what = 1;
-                    msg.obj = bitmap1;
-                    handler.sendMessage(msg);
-                }
-            }.start();
-
+        News news = realm.where(News.class).equalTo("newsID", newsID).findFirst();
+        if (news != null) {
+            title_view.setText(news.title);
+            content_view.setText(news.content);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            date_view.setText(format.format(news.publishTime));
+            if (news.images != null && news.images.size() > 0) {
+                Glide.with(this).load(news.images.get(0)).into(image_view);
+            }
         }
 
     }
     // TODO: share link; news recommendation; show multiple images
-
-    private Bitmap getBitMap(String url)
-    {
-        Bitmap bitmap=null;
-        URL picUrl=null;
-        try {
-            picUrl=new URL(url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        try {
-            HttpURLConnection connection=(HttpURLConnection)picUrl.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream inputStream=connection.getInputStream();
-            bitmap= BitmapFactory.decodeStream(inputStream);
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return bitmap;
-    }
-
 }
