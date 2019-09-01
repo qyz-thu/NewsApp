3e7f516320cd0481c9bfb1d80668619fe7257393
qyz17 <qyz17@mails.tsinghua.edu.cn>
2019-09-01 21:23:51 +0800

    show image; add date

--- app/src/main/java/com/example/newsapp/NewsDetailActivity.java
+++ app/src/main/java/com/example/newsapp/NewsDetailActivity.java
@@ -2,8 +2,12 @@ package com.example.newsapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.os.Message;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,12 +16,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 import com.example.newsapp.model.News;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+
 import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class NewsDetailActivity extends AppCompatActivity {
     TextView title_view;
     TextView content_view;
+    TextView date_view;
     ImageView image_view;
 
     @Override
@@ -31,6 +43,7 @@ public class NewsDetailActivity extends AppCompatActivity {
         title_view = findViewById(R.id.news_title);
         image_view = findViewById(R.id.news_image);
         content_view = findViewById(R.id.news_content);
+        date_view = findViewById(R.id.news_date);
 
         if (intent != null)
         {
@@ -45,7 +58,55 @@ public class NewsDetailActivity extends AppCompatActivity {
         }
         title_view.setText(news.title);
         content_view.setText(news.content);
-        // TODO: show image; share link; news recommendation
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        date_view.setText(format.format(news.publishTime));
+        final String url = news.images.get(0);
+        if (url != null && url.length()>0) {
+            final Handler handler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    if (msg.what == 1) {
+                        Bitmap bitmap2 = (Bitmap) msg.obj;
+                        image_view.setImageBitmap(bitmap2);
+                    }
+                }
+            };
+
+            new Thread() {
+                public void run() {
+                    Bitmap bitmap1 = getBitMap(url);
+                    Message msg = new Message();
+                    msg.what = 1;
+                    msg.obj = bitmap1;
+                    handler.sendMessage(msg);
+                }
+            }.start();
+
+        }
 
     }
+    // TODO: share link; news recommendation; show multiple images
+
+    private Bitmap getBitMap(String url)
+    {
+        Bitmap bitmap=null;
+        URL picUrl=null;
+        try {
+            picUrl=new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            HttpURLConnection connection=(HttpURLConnection)picUrl.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream inputStream=connection.getInputStream();
+            bitmap= BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
 }
